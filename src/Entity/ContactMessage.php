31e547030ba03e3a1f13a2980b10a749d5e28d8a
Jérémy GABOURG <gabourg.developpement@gmail.com>
2022-04-04 22:54:29 +0200

    Add ContactMessage forgotten entity

---
+++ src/Entity/ContactMessage.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactMessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactMessageRepository::class)]
+class ContactMessage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $userLastName;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $userFirstName;
+
+    #[ORM\Column(type: 'boolean')]
+    private $userIsAlreadyClient;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $subject;
+
+    #[ORM\Column(type: 'text')]
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserLastName(): ?string
+    {
+        return $this->userLastName;
+    }
+
+    public function setUserLastName(string $userLastName): self
+    {
+        $this->userLastName = $userLastName;
+
+        return $this;
+    }
+
+    public function getUserFirstName(): ?string
+    {
+        return $this->userFirstName;
+    }
+
+    public function setUserFirstName(string $userFirstName): self
+    {
+        $this->userFirstName = $userFirstName;
+
+        return $this;
+    }
+
+    public function getUserIsAlreadyClient(): ?bool
+    {
+        return $this->userIsAlreadyClient;
+    }
+
+    public function setUserIsAlreadyClient(bool $userIsAlreadyClient): self
+    {
+        $this->userIsAlreadyClient = $userIsAlreadyClient;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
