b19e454d8f8023c290cd16e855062309422ea15d
Jérémy GABOURG <gabourg.developpement@gmail.com>
2022-04-07 06:17:04 +0200

    Add font directory in assets

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.css';
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
