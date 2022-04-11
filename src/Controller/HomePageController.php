ccab1b740ea9b5351d584863932762b206ca80bc
Jérémy GABOURG <gabourg.developpement@gmail.com>
2022-04-11 22:25:13 +0200

    Modify 'use' list in HomepageController

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -7,7 +7,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Media;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomePageController extends AbstractController
 {
