cbd1a9c2c76b183fa63303ca1eb246a4f3d5899a
Jérémy GABOURG <gabourg.developpement@gmail.com>
2022-04-21 19:09:36 +0200

    Improve checkpassword.js

--- assets/js/checkPassword.js
+++ assets/js/checkPassword.js
@@ -1,11 +1,10 @@
-let firstPass = document.getElementById('password');
+let firstPass = document.getElementById('registration_form_plainPassword');
 let verifyPass = document.getElementById('verifpass');
 
 verifyPass.addEventListener('blur', (event)=>{
-    if(firstPass.value === event.target.value) {
+    if(firstPass.value !== null && verifyPass !== null && firstPass.value !== verifyPass.value) {
         //implémntez votre code
-        alert("Les deux mots de passe saisies sont différents");
-        alert("Merci de renouveler l'opération");
+        alert("Les deux mots de passe saisies sont différents. Merci de renouveler l'opération");
         return false;
     }
 })
